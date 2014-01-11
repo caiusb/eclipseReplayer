60eec0f924ac60c9d9c7539b915841d1e1c42c15
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-01-10 16:14:49 -0800

    Built a new LauchedApplicationOperation that successfully launches the configuration

--- edu.oregonstate.cope.eclipse.operations/src/edu/illinois/codingtracker/operations/starts/LaunchedApplicationOperation.java
+++ edu.oregonstate.cope.eclipse.operations/src/edu/illinois/codingtracker/operations/starts/LaunchedApplicationOperation.java
@@ -3,16 +3,27 @@
  */
 package edu.illinois.codingtracker.operations.starts;
 
-import static edu.oregonstate.cope.clientRecorder.JSONConstants.JSON_LAUNCH_ATTRIBUTES;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
 import org.json.simple.JSONObject;
 
 import edu.illinois.codingtracker.operations.OperationLexer;
 import edu.illinois.codingtracker.operations.OperationSymbols;
 import edu.illinois.codingtracker.operations.OperationTextChunk;
 import edu.illinois.codingtracker.operations.UserOperation;
+import edu.oregonstate.cope.clientRecorder.Events;
+import edu.oregonstate.cope.clientRecorder.JSONConstants;
 
 /**
  * 
@@ -22,6 +33,17 @@ import edu.illinois.codingtracker.operations.UserOperation;
 public class LaunchedApplicationOperation extends UserOperation {
 	
 	private Map launchAttributes;
+	private String launchConfig;
+	private String launchMode;
+	private String launchName;
+	private String launchFile;
+	
+	public LaunchedApplicationOperation(String mode) {
+		if (mode.equals(Events.debugLaunch.toString()))
+			launchMode = ILaunchManager.DEBUG_MODE;
+		if (mode.equals(Events.normalLaunch.toString()))
+			launchMode = ILaunchManager.RUN_MODE;
+	}
 
 	@Override
 	protected char getOperationSymbol() {
@@ -43,12 +65,29 @@ public class LaunchedApplicationOperation extends UserOperation {
 	
 	@Override
 	public void parse(JSONObject value) {
-		launchAttributes = (Map) value.get(JSON_LAUNCH_ATTRIBUTES);
+		launchAttributes = (Map) value.get(JSONConstants.JSON_LAUNCH_ATTRIBUTES);
+		launchConfig = (String) value.get(JSONConstants.JSON_LAUNCH_CONFIGURATION);
+		launchName = (String) value.get(JSONConstants.JSON_LAUNCH_NAME);
+		launchFile = (String) value.get(JSONConstants.JSON_LAUNCH_FILE);
 	}
 
 	@Override
 	public void replay() throws Exception {
-		
+		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+		writeLaunchFile(launchName, launchFile);
+		ILaunchConfiguration launchConfiguration = launchManager.getLaunchConfiguration(launchConfig);
+		ILaunchConfigurationWorkingCopy launchConfigWorkingCopy = launchConfiguration.getWorkingCopy();
+		launchConfigWorkingCopy.setAttributes(launchAttributes);
+		ILaunchConfiguration newLaunchConfig = launchConfigWorkingCopy.doSave();
+		ILaunch launch = newLaunchConfig.launch(launchMode, new NullProgressMonitor(), true);
+	}
+
+	private void writeLaunchFile(String launchName, String launchFileContents) {
+		try {
+			Files.write(Paths.get(ResourcesPlugin.getWorkspace().getRoot().getLocation().makeAbsolute().toPortableString(), ".metadata/.plugins/org.eclipse.debug.core/.launches/" + launchName + ".launch"), launchFileContents.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
 
 	@Override
